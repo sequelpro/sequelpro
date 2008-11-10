746ab0090f0a11014ee899bc2922f42d60c22f4e
avenjamin <avenjamin@gmail.com>
2008-11-10 15:42:04 +0000

    Random cleanup of this file

--- new_xcode_project/CMMCPResult.m
+++ new_xcode_project/CMMCPResult.m
@@ -32,14 +32,14 @@
 modified version for use with sequel-pro
 "*/
 {
-	MYSQL_ROW			theRow;
-	unsigned long		*theLengths;
-	MYSQL_FIELD			*theField;
+	MYSQL_ROW		theRow;
+	unsigned long	*theLengths;
+	MYSQL_FIELD		*theField;
 	int				i;
 	id				theReturn;
 
 	if (mResult == NULL) {
-// If there is no results, returns nil, as after the last row...
+		// If there is no results, returns nil, as after the last row...
 		return nil;
 	}
 
@@ -66,6 +66,7 @@ modified version for use with sequel-pro
 
 	theLengths = mysql_fetch_lengths(mResult);
 	theField = mysql_fetch_fields(mResult);
+	
 	for (i=0; i<mNumOfFields; i++) {
 		id	theCurrentObj;
 
@@ -74,7 +75,7 @@ modified version for use with sequel-pro
 		}
 		else {
 			char	*theData = calloc(sizeof(char),theLengths[i]+1);
-//			   char  *theUselLess;
+			// char  *theUselLess;
 			memcpy(theData, theRow[i],theLengths[i]);
 			theData[theLengths[i]] = '\0';
 
@@ -88,17 +89,16 @@ modified version for use with sequel-pro
 				case FIELD_TYPE_FLOAT:
 				case FIELD_TYPE_DOUBLE:
 				case FIELD_TYPE_NEW_DECIMAL:					
-					theCurrentObj = [self stringWithCString:theData];
-					break;
 				case FIELD_TYPE_TIMESTAMP:
 				case FIELD_TYPE_DATE:
 				case FIELD_TYPE_TIME:
 				case FIELD_TYPE_DATETIME:
 				case FIELD_TYPE_YEAR:
-					theCurrentObj = [self stringWithCString:theData];
-					break;
 				case FIELD_TYPE_VAR_STRING:
 				case FIELD_TYPE_STRING:
+				case FIELD_TYPE_SET:
+				case FIELD_TYPE_ENUM:
+				case FIELD_TYPE_NEWDATE: // Don't know what the format for this type is...
 					theCurrentObj = [self stringWithCString:theData];
 					break;
 				case FIELD_TYPE_TINY_BLOB:
@@ -106,42 +106,33 @@ modified version for use with sequel-pro
 				case FIELD_TYPE_MEDIUM_BLOB:
 				case FIELD_TYPE_LONG_BLOB:
 					theCurrentObj = [NSData dataWithBytes:theData length:theLengths[i]];
-				   if (!(theField[i].flags & BINARY_FLAG)) { // It is TEXT and NOT BLOB...
-					  theCurrentObj = [self stringWithText:theCurrentObj];
-				   }
-//#warning Should check for TEXT (using theField[i].flag BINARY_FLAG)
+					if (!(theField[i].flags & BINARY_FLAG)) { // It is TEXT and NOT BLOB...
+						theCurrentObj = [self stringWithText:theCurrentObj];
+					} //#warning Should check for TEXT (using theField[i].flag BINARY_FLAG)
 					break;
-				case FIELD_TYPE_SET:
-					theCurrentObj = [self stringWithCString:theData];
-					break;
-				case FIELD_TYPE_ENUM:
-					theCurrentObj = [self stringWithCString:theData];
-					break;
+
 				case FIELD_TYPE_NULL:
-				   theCurrentObj = [NSNull null];
+					theCurrentObj = [NSNull null];
 					break;
-				case FIELD_TYPE_NEWDATE:
-// Don't know what the format for this type is...
-					theCurrentObj = [self stringWithCString:theData];
-					break;
+
 				default:
-					NSLog (@"in fetchRowAsDictionary : Unknown type : %d for column %d, send back a NSData object", (int)theField[i].type, (int)i);
+					NSLog (@"in fetchRowAsType : Unknown type : %d for column %d, send back a NSData object", (int)theField[i].type, (int)i);
 					theCurrentObj = [NSData dataWithBytes:theData length:theLengths[i]];
 					break;
 			}
+			
 			free(theData);
-// Some of the creators return nil object...
+			// Some of the creators return nil object...
 			if (theCurrentObj == nil) {
 				theCurrentObj = [NSNull null];
 			}
 		}
+		
 		switch (aType) {
-			case MCPTypeArray :
-				[theReturn addObject:theCurrentObj];
-				break;
 			case MCPTypeDictionary :
 				[theReturn setObject:theCurrentObj forKey:[mNames objectAtIndex:i]];
 				break;
+			case MCPTypeArray :
 			default :
 				[theReturn addObject:theCurrentObj];
 				break;
