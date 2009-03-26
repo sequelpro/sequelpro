5aaadd3f2d83ada69d60b30fbe91888777fbc3dd
bamse16 <marius@marius.me.uk>
2009-03-26 13:14:58 +0000

    Enabled Page Setup for printing, so now you can print landscape too Enabled auto pagination, so we print everything for now

--- Source/TableDocument.m
+++ Source/TableDocument.m
@@ -92,10 +92,11 @@ NSString *TableDocumentFavoritesControllerFavoritesDidChange = @"TableDocumentFa
 {
 	
 	NSPrintInfo *printInfo = [self printInfo];
+	[printInfo setHorizontalPagination:NSAutoPagination];
+	[printInfo setVerticalPagination:NSAutoPagination];
 	NSPrintOperation *printOp = [NSPrintOperation printOperationWithView:[[tableTabView selectedTabViewItem] view] printInfo:printInfo];
 	return printOp;
 }
-
 
 - (CMMCPConnection *)sharedConnection
 {
