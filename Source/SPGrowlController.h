40a49b30a8feaf8f0cdd7a084d9843ce7d6da77f
stuconnolly <stuart02@gmail.com>
2009-04-11 15:49:04 +0000

    Implement very basic Growl enable/disable preference. Still need to add more complex notification rules.

--- Source/SPGrowlController.h
+++ Source/SPGrowlController.h
@@ -30,5 +30,6 @@
 
 // Post notification
 - (void)notifyWithTitle:(NSString *)title description:(NSString *)description notificationName:(NSString *)name;
+- (void)notifyWithTitle:(NSString *)title description:(NSString *)description notificationName:(NSString *)name iconData:(NSData *)data priority:(int)priority isSticky:(BOOL)sticky clickContext:(id)clickContext;
 
 @end
