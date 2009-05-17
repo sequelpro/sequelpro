c4ca14a1ba9c6ec7f1f63f5f23208b31560ecd36
stuconnolly <stuart02@gmail.com>
2009-05-17 18:08:44 +0000

    Query console save panel shouldn't be being run twice.

--- Source/SPQueryConsole.m
+++ Source/SPQueryConsole.m
@@ -192,12 +192,6 @@ static SPQueryConsole *sharedQueryConsole = nil;
 	[panel setAccessoryView:saveLogView];
 
 	[panel beginSheetForDirectory:nil file:DEFAULT_CONSOLE_LOG_FILENAME modalForWindow:[self window] modalDelegate:self didEndSelector:@selector(savePanelDidEnd:returnCode:contextInfo:) contextInfo:NULL];
-	[panel beginSheetForDirectory:nil 
-							 file:DEFAULT_CONSOLE_LOG_FILENAME 
-				   modalForWindow:[self window] 
-					modalDelegate:self 
-				   didEndSelector:@selector(savePanelDidEnd:returnCode:contextInfo:) 
-					  contextInfo:NULL];
 }
 
 /**
