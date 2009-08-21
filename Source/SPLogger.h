68bbc4aae63e38eae25236c80dbc08896537755e
rowanbeentje <rowan@beent.je>
2009-08-21 00:03:16 +0000

    - Add a "SPLogger" class to allow easy debug builds when attempting to pinpoint the cause of specific user issues; see SPLogger.m for very brief usage notes

---
+++ Source/SPLogger.h
@@ -0,0 +1,39 @@
+//
+//  $Id$
+//
+//  SPLogger.h
+//  sequel-pro
+//
+//  Created by Rowan Beentje on 17/06/2009.
+//  Copyright 2009 Rowan Beentje. All rights reserved.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  More info at <http://code.google.com/p/sequel-pro/>
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SPLogger : NSObject {
+	BOOL initializedSuccessfully;
+	NSFileHandle *logFileHandle;
+}
+
++ (SPLogger *)logger;
+
+- (void) outputTimeString;
+- (void) log:(NSString *)theString, ...;
+
+@end
