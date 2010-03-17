c4c24a3b54abb9fcab76bf204535e64bb2b0f61c
stuconnolly <stuart02@gmail.com>
2010-03-17 16:51:00 +0000

    Add the running of genstrings(1) at the start of the general build script to update Localizable.strings to ensure it is always up to date. This addresses the second point of issue #593. Also, add a bunch of status messages to the script to provide feedback on the current operation.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -13,18 +13,29 @@
 
 BUILD_PRODUCT="${BUILT_PRODUCTS_DIR}/${TARGET_NAME}${WRAPPER_SUFFIX}"
 
+echo "Running genstrings to update 'Localizable.strings'..."
+
+# Update 'Localizable.strings' by running genstrings(1)
+genstrings -o "${SRCROOT}/Resources/English.lproj" "${SRCROOT}/Source/*.m"
+
+echo 'Updating build version...'
+
 # Add the build/bundle version
 "${SRCROOT}/Scripts/build-version.pl"
 
 # Trim the application if this is a 'Release' or 'Distribution' build
 if [[ "$CONFIGURATION" == 'Release' || "$CONFIGURATION" == 'Distribution' ]]
 then
+	echo 'Running trim-application.sh to strip application resources for distribution...'
+
 	"${SRCROOT}/Scripts/trim-application.sh" -p "$BUILD_PRODUCT" -a
 fi
 
 # Perform distribution specific tasks if this is a 'Distribution' build
 if [ "$CONFIGURATION" == 'Distribution' ]
 then
+	echo 'Performing distribution build code signing...'
+
 	codesign -s 'Sequel Pro Distribution' "${BUILD_PRODUCT}/Contents/Resources/SequelProTunnelAssistant" 2> /dev/null
 	codesign -s 'Sequel Pro Distribution' "${BUILD_PRODUCT}" 2> /dev/null
 	
@@ -34,10 +45,13 @@ then
 	
 	if [ "$VERIFYERRORS" != '' ]
 	then
-		echo "error: Signing verification threw an error: $VERIFYERRORS"
-		echo "error: All distribution builds must be signed with the key used for all previous distribution signing!"
+		echo "Error: Signing verification threw an error: $VERIFYERRORS"
+		echo "Error: All distribution builds must be signed with the key used for all previous distribution signing!"
+		
 		exit 1
 	fi
+	
+	echo 'Running package-application.sh to package application for distribution...'
 
 	"${SRCROOT}/Scripts/package-application.sh" -p "$BUILD_PRODUCT"
 fi
@@ -45,6 +59,8 @@ fi
 # Development build code signing
 if [ "$CONFIGURATION" == 'Debug' ]
 then
+	echo 'Performing development build code signing...'
+
 	codesign -s 'Sequel Pro Development' "${BUILD_PRODUCT}/Contents/Resources/SequelProTunnelAssistant" 2> /dev/null
 	codesign -s 'Sequel Pro Development' "$BUILD_PRODUCT" 2> /dev/null
 	
