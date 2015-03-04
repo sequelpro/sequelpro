ed38f229a052a678d3a5022afd3806b1c3b434cf
Max <post@wickenrode.com>
2015-03-04 01:44:12 +0100

    !!! Update mysql client libs to 5.5.42
    This enables SHA256 client certificates (issue #2071) and disables SSL2 and SSL3 connections.
    Please test carefully!

--- Frameworks/SPMySQLFramework/MySQL Client Libraries/include/mysql_version.h
+++ Frameworks/SPMySQLFramework/MySQL Client Libraries/include/mysql_version.h
@@ -1,6 +1,7 @@
-/* Copyright (c) 1996, 1999-2004, 2007 MySQL AB
-   Use is subject to license terms
-   This file is public domain and comes with NO WARRANTY of any kind */
+/* Copyright Abandoned 1996,1999 TCX DataKonsult AB & Monty Program KB
+   & Detron HB, 1996, 1999-2004, 2007 MySQL AB.
+   This file is public domain and comes with NO WARRANTY of any kind
+*/
 
 /* Version numbers for protocol & mysqld */
 
@@ -10,11 +11,11 @@
 #include <custom_conf.h>
 #else
 #define PROTOCOL_VERSION		10
-#define MYSQL_SERVER_VERSION		"5.5.33"
+#define MYSQL_SERVER_VERSION		"5.5.42"
 #define MYSQL_BASE_VERSION		"mysqld-5.5"
 #define MYSQL_SERVER_SUFFIX_DEF		""
 #define FRM_VER				6
-#define MYSQL_VERSION_ID		50533
+#define MYSQL_VERSION_ID		50542
 #define MYSQL_PORT			3306
 #define MYSQL_PORT_DEFAULT		0
 #define MYSQL_UNIX_ADDR			"/tmp/mysql.sock"
