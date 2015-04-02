01dd6402ee530e5d9fff1916507ea287893f0960
Max <post@wickenrode.com>
2015-04-02 17:51:00 +0200

    Some code reformatting

--- Source/SPTableContentDataSource.m
+++ Source/SPTableContentDataSource.m
@@ -54,96 +54,92 @@
 	if (tableView == filterTableView) {
 		return filterTableIsSwapped ? [filterTableData count] : [[[filterTableData objectForKey:@"0"] objectForKey:SPTableContentFilterKey] count];
 	}
-	else 
 #endif
-		if (tableView == tableContentView) {
-			return tableRowsCount;
-		}
+	if (tableView == tableContentView) {
+		return tableRowsCount;
+	}
 	
 	return 0;
 }
 
 - (id)tableView:(SPCopyTable *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)rowIndex
 {
+	NSUInteger columnIndex = [[tableColumn identifier] integerValue];
 #ifndef SP_CODA
 	if (tableView == filterTableView) {
-		if (filterTableIsSwapped)
-			
+		if (filterTableIsSwapped) {
 			// First column shows the field names
-			if ([[tableColumn identifier] integerValue] == 0) {
+			if (columnIndex == 0) {
 				return [[[NSTableHeaderCell alloc] initTextCell:[[filterTableData objectForKey:[NSNumber numberWithInteger:rowIndex]] objectForKey:@"name"]] autorelease];
 			} 
-			else {
-				return NSArrayObjectAtIndex([[filterTableData objectForKey:[NSNumber numberWithInteger:rowIndex]] objectForKey:SPTableContentFilterKey], [[tableColumn identifier] integerValue] - 1);
-			}
-		else {
-			return NSArrayObjectAtIndex([[filterTableData objectForKey:[tableColumn identifier]] objectForKey:SPTableContentFilterKey], rowIndex);
+
+			return NSArrayObjectAtIndex([[filterTableData objectForKey:[NSNumber numberWithInteger:rowIndex]] objectForKey:SPTableContentFilterKey], columnIndex - 1);
 		}
+		
+		return NSArrayObjectAtIndex([[filterTableData objectForKey:[tableColumn identifier]] objectForKey:SPTableContentFilterKey], rowIndex);
 	}
-	else 
 #endif
-		if (tableView == tableContentView) {
+	if (tableView == tableContentView) {
+		
+		id value = nil;
+		
+		// While the table is being loaded, additional validation is required - data
+		// locks must be used to avoid crashes, and indexes higher than the available
+		// rows or columns may be requested.  Return "..." to indicate loading in these
+		// cases.
+		if (isWorking) {
+			pthread_mutex_lock(&tableValuesLock);
 			
-			id value = nil;
-			NSUInteger columnIndex = [[tableColumn identifier] integerValue];
+			if (rowIndex < (NSInteger)tableRowsCount && columnIndex < [tableValues columnCount]) {
+				value = [self _contentValueForTableColumn:columnIndex row:rowIndex asPreview:YES];
+			}
 			
-			// While the table is being loaded, additional validation is required - data
-			// locks must be used to avoid crashes, and indexes higher than the available
-			// rows or columns may be requested.  Return "..." to indicate loading in these
-			// cases.
-			if (isWorking) {
-				pthread_mutex_lock(&tableValuesLock);
-				
-				if (rowIndex < (NSInteger)tableRowsCount && columnIndex < [tableValues columnCount]) {
-					value = [self _contentValueForTableColumn:columnIndex row:rowIndex asPreview:YES];
-				}
-				
-				pthread_mutex_unlock(&tableValuesLock);
-				
-				if (!value) return @"...";
-			} 
+			pthread_mutex_unlock(&tableValuesLock);
+			
+			if (!value) return @"...";
+		} 
+		else {
+			if ([tableView editedColumn] == (NSInteger)columnIndex && [tableView editedRow] == rowIndex) {
+				value = [self _contentValueForTableColumn:columnIndex row:rowIndex asPreview:NO];
+			}
 			else {
-				if ([tableView editedColumn] == (NSInteger)columnIndex && [tableView editedRow] == rowIndex) {
-					value = [self _contentValueForTableColumn:columnIndex row:rowIndex asPreview:NO];
-				}
-				else {
-					value = [self _contentValueForTableColumn:columnIndex row:rowIndex asPreview:YES];
-				}
+				value = [self _contentValueForTableColumn:columnIndex row:rowIndex asPreview:YES];
 			}
-
-			NSDictionary *columnDefinition = [[(id <SPDatabaseContentViewDelegate>)[tableContentView delegate] dataColumnDefinitions] objectAtIndex:columnIndex];
-
-			NSString *columnType = [columnDefinition objectForKey:@"typegrouping"];
-			
-			if ([value isKindOfClass:[SPMySQLGeometryData class]]) {
-				return [value wktString];
-			}
-			
-			if ([value isNSNull]) {
-				return [prefs objectForKey:SPNullValue];
-			}
-			
-			if ([value isKindOfClass:[NSData class]]) {
-
-				if ([columnType isEqualToString:@"binary"] && [prefs boolForKey:SPDisplayBinaryDataAsHex]) {
-					return [NSString stringWithFormat:@"0x%@", [value dataToHexString]];
-				}
-
-				pthread_mutex_t *fieldEditorCheckLock = NULL;
-				if (isWorking) {
-					fieldEditorCheckLock = &tableValuesLock;
-				}
-
-				// Always retrieve the short string representation, truncating the value where necessary
-				return [value shortStringRepresentationUsingEncoding:[mySQLConnection stringEncoding]];
-			}
-			
-			if ([value isSPNotLoaded]) {
-				return NSLocalizedString(@"(not loaded)", @"value shown for hidden blob and text fields");
-			}
-			
-			return value;
 		}
+
+		NSDictionary *columnDefinition = [[(id <SPDatabaseContentViewDelegate>)[tableContentView delegate] dataColumnDefinitions] objectAtIndex:columnIndex];
+
+		NSString *columnType = [columnDefinition objectForKey:@"typegrouping"];
+		
+		if ([value isKindOfClass:[SPMySQLGeometryData class]]) {
+			return [value wktString];
+		}
+		
+		if ([value isNSNull]) {
+			return [prefs objectForKey:SPNullValue];
+		}
+		
+		if ([value isKindOfClass:[NSData class]]) {
+
+			if ([columnType isEqualToString:@"binary"] && [prefs boolForKey:SPDisplayBinaryDataAsHex]) {
+				return [NSString stringWithFormat:@"0x%@", [value dataToHexString]];
+			}
+
+			pthread_mutex_t *fieldEditorCheckLock = NULL;
+			if (isWorking) {
+				fieldEditorCheckLock = &tableValuesLock;
+			}
+
+			// Always retrieve the short string representation, truncating the value where necessary
+			return [value shortStringRepresentationUsingEncoding:[mySQLConnection stringEncoding]];
+		}
+		
+		if ([value isSPNotLoaded]) {
+			return NSLocalizedString(@"(not loaded)", @"value shown for hidden blob and text fields");
+		}
+		
+		return value;
+	}
 	
 	return nil;
 }
@@ -163,49 +159,48 @@
 		
 		return;
 	}
-	else 
 #endif
-		if (tableView == tableContentView) {
-			
-			// If the current cell should have been edited in a sheet, do nothing - field closing will have already
-			// updated the field.
-			if ([tableContentView shouldUseFieldEditorForRow:rowIndex column:[[tableColumn identifier] integerValue] checkWithLock:NULL]) {
-				return;
-			}
-			
-			// If table data comes from a view, save back to the view
-			if ([tablesListInstance tableType] == SPTableTypeView) {
-				[self saveViewCellValue:object forTableColumn:tableColumn row:rowIndex];
-				return;
-			}
-			
-			// Catch editing events in the row and if the row isn't currently being edited,
-			// start an edit.  This allows edits including enum changes to save correctly.
-			if (isEditingRow && [tableContentView selectedRow] != currentlyEditingRow) {
-				[self saveRowOnDeselect];
-			}
-			
-			if (!isEditingRow) {
-				[oldRow setArray:[tableValues rowContentsAtIndex:rowIndex]];
-				
-				isEditingRow = YES;
-				currentlyEditingRow = rowIndex;
-			}
-			
-			NSDictionary *column = NSArrayObjectAtIndex(dataColumns, [[tableColumn identifier] integerValue]);
-			
-			if (object) {
-				// Restore NULLs if necessary
-				if ([object isEqualToString:[prefs objectForKey:SPNullValue]] && [[column objectForKey:@"null"] boolValue]) {
-					object = [NSNull null];
-				}
-				
-				[tableValues replaceObjectInRow:rowIndex column:[[tableColumn identifier] integerValue] withObject:object];
-			} 
-			else {
-				[tableValues replaceObjectInRow:rowIndex column:[[tableColumn identifier] integerValue] withObject:@""];
-			}
+	if (tableView == tableContentView) {
+		
+		// If the current cell should have been edited in a sheet, do nothing - field closing will have already
+		// updated the field.
+		if ([tableContentView shouldUseFieldEditorForRow:rowIndex column:[[tableColumn identifier] integerValue] checkWithLock:NULL]) {
+			return;
 		}
+		
+		// If table data comes from a view, save back to the view
+		if ([tablesListInstance tableType] == SPTableTypeView) {
+			[self saveViewCellValue:object forTableColumn:tableColumn row:rowIndex];
+			return;
+		}
+		
+		// Catch editing events in the row and if the row isn't currently being edited,
+		// start an edit.  This allows edits including enum changes to save correctly.
+		if (isEditingRow && [tableContentView selectedRow] != currentlyEditingRow) {
+			[self saveRowOnDeselect];
+		}
+		
+		if (!isEditingRow) {
+			[oldRow setArray:[tableValues rowContentsAtIndex:rowIndex]];
+			
+			isEditingRow = YES;
+			currentlyEditingRow = rowIndex;
+		}
+		
+		NSDictionary *column = NSArrayObjectAtIndex(dataColumns, [[tableColumn identifier] integerValue]);
+		
+		if (object) {
+			// Restore NULLs if necessary
+			if ([object isEqualToString:[prefs objectForKey:SPNullValue]] && [[column objectForKey:@"null"] boolValue]) {
+				object = [NSNull null];
+			}
+			
+			[tableValues replaceObjectInRow:rowIndex column:[[tableColumn identifier] integerValue] withObject:object];
+		} 
+		else {
+			[tableValues replaceObjectInRow:rowIndex column:[[tableColumn identifier] integerValue] withObject:@""];
+		}
+	}
 }
 
 @end
