sequelpro/sequelpro

commit ce3353422006e927be3005123223c20d8e09b539

Use find bar in field editor on 10.7+
This will IMHO obliberate the need for the field editor to become a main window and avoid the UI clash from #2148

Source/SPFieldEditorController.m
@@ -40,6 +40,13 @@
 
 #import <SPMySQL/SPMySQL.h>
 
+#if __MAC_OS_X_VERSION_MAX_ALLOWED < __MAC_10_7
+@interface NSTextView (LionPlus)
+- (void)setUsesFindBar:(BOOL)value;
+- (BOOL)usesFindBar;
+@end
+#endif
+
 typedef enum {
 	TextSegment = 0,
 	ImageSegment,
@@ -93,9 +100,18 @@ typedef enum {
 		// Allow the user to enter cmd+return to close the edit sheet in addition to fn+return
 		[editSheetOkButton setKeyEquivalentModifierMask:NSCommandKeyMask];
 
-		// Permit the field edit sheet to become main if necessary; this allows fields within the sheet to
-		// support full interactivity, for example use of the NSFindPanel inside NSTextViews.
-		[editSheet setIsSheetWhichCanBecomeMain:YES];
+		if([editTextView respondsToSelector:@selector(setUsesFindBar:)])
+			// 10.7+
+			// Stealing the main window from the actual main window will cause
+			// a UI bug with the tab bar and the find panel was really the only
+			// thing that had an issue with not working with sheets.
+			// The find bar works fine without hackery.
+			[editTextView setUsesFindBar:YES];
+		else {
+			// Permit the field edit sheet to become main if necessary; this allows fields within the sheet to
+			// support full interactivity, for example use of the NSFindPanel inside NSTextViews.
+			[editSheet setIsSheetWhichCanBecomeMain:YES];
+		}
 
 		allowUndo = NO;
 		selectionChanged = NO;

commit dc1adcaafc3439c1998fdfaf15f0cdeee9be778f

Fix an issue on 10.7+ introduced in ce33534
This removes a IMHO useless makeFirstResponder: call. In all cases where [NSApp mainWindow] == usedSheet (< 10.7) the call right above it would already handle it. On the other hand if [NSApp mainWindow] != usedSheet (10.7+) the call would fail anyways because in that case editTextView is not even a subview of the main window.

Frameworks/SPMySQLFramework/Source/SPMySQLConnection.m
@@ -418,7 +418,7 @@ const char *SPMySQLSSLPermissibleCiphers = "DHE-RSA-AES256-SHA:AES256-SHA:DHE-RS
 
 	// If a connection is already active in some form, throw an exception
 	if (state != SPMySQLDisconnected && state != SPMySQLConnectionLostInBackground) {
-		[NSException raise:NSInternalInconsistencyException format:@"Attempted to connect a connection that is not disconnected (%d).", state];
+		[NSException raise:NSInternalInconsistencyException format:@"Attempted to connect a connection that is not disconnected (SPMySQLConnectionState=%d).", state];
 		return NO;
 	}
 	state = SPMySQLConnecting;

Source/SPFieldEditorController.m
@@ -524,9 +524,6 @@ typedef enum {
 			[hexTextView setHidden:YES];
 			[hexTextScrollView setHidden:YES];
 			[usedSheet makeFirstResponder:editTextView];
-#ifndef SP_CODA
-			[[NSApp mainWindow] makeFirstResponder:editTextView];
-#endif
 			break;
 		case ImageSegment:
 			[editTextView setHidden:YES];