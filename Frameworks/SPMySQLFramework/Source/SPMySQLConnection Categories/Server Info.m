sequelpro/sequelpro

commit b2d798ba9282d3acf1a2d65de30849e529d4d255

* Lock connection during disconnect to prevent some race conditions
* Always use the server version name provided by mysql_get_server_info() as that should me more reliable
* Use mysql_get_server_version() for version comparisons. Less code, official API and closer to what mysql does

Frameworks/SPMySQLFramework/Source/SPMySQLConnection Categories/Server Info.m
@@ -46,54 +46,34 @@
 		return [NSString stringWithString:serverVariableVersion];
 	}
 
-#warning FIXME: There is probably a race condition here with -[self _disconnect]
-	if(mySQLConnection) {
-		return [self _stringForCString:mysql_get_server_info(mySQLConnection)];
-	}
-	
 	return nil;
 }
 
 /**
- * Return the server major version or NSNotFound on failure
+ * Return the server major version or 0 on failure
  */
 - (NSUInteger)serverMajorVersion
 {
-	NSString *ver;
-	if ((ver = [self serverVersionString]) != nil) {
-		NSString *s = [[ver componentsSeparatedByString:@"."] objectAtIndex:0];
-		return (NSUInteger)[s integerValue];
-	} 
-	
-	return NSNotFound;
+	// 5.5.33 => 50533 / 10'000 => 5.0533 => 5
+	return (serverVersionNumber / 10000);
 }
 
 /**
- * Return the server minor version or NSNotFound on failure
+ * Return the server minor version or 0 on failure
  */
 - (NSUInteger)serverMinorVersion
 {
-	NSString *ver;
-	if ((ver = [self serverVersionString]) != nil) {
-		NSString *s = [[ver componentsSeparatedByString:@"."] objectAtIndex:1];
-		return (NSUInteger)[s integerValue];
-	}
-	
-	return NSNotFound;
+	// 5.5.33 => 50533 - (5*10'000) => 533 / 100 => 5.33 => 5
+	return ((serverVersionNumber - [self serverMajorVersion]*10000) / 100);
 }
 
 /**
- * Return the server release version or NSNotFound on failure
+ * Return the server release version or 0 on failure
  */
 - (NSUInteger)serverReleaseVersion
 {
-	NSString *ver;
-	if ((ver = [self serverVersionString]) != nil) {
-		NSString *s = [[ver componentsSeparatedByString:@"."] objectAtIndex:2];
-		return (NSUInteger)[[[s componentsSeparatedByString:@"-"] objectAtIndex:0] integerValue];
-	}
-	
-	return NSNotFound;
+	// 5.5.33 => 50533 - (5*10'000 + 5*100) => 33
+	return (serverVersionNumber - ([self serverMajorVersion]*10000 + [self serverMinorVersion]*100));
 }
 
 #pragma mark -
@@ -105,23 +85,9 @@
  */
 - (BOOL)serverVersionIsGreaterThanOrEqualTo:(NSUInteger)aMajorVersion minorVersion:(NSUInteger)aMinorVersion releaseVersion:(NSUInteger)aReleaseVersion
 {
-	NSString *ver;
-	if (!(ver = [self serverVersionString])) return NO;
+	unsigned long myver = aMajorVersion * 10000 + aMinorVersion * 100 + aReleaseVersion;
 
-	NSArray *serverVersionParts = [ver componentsSeparatedByString:@"."];
-
-	NSUInteger serverMajorVersion = (NSUInteger)[[serverVersionParts objectAtIndex:0] integerValue];
-	if (serverMajorVersion < aMajorVersion) return NO;
-	if (serverMajorVersion > aMajorVersion) return YES;
-
-	NSUInteger serverMinorVersion = (NSUInteger)[[serverVersionParts objectAtIndex:1] integerValue];
-	if (serverMinorVersion < aMinorVersion) return NO;
-	if (serverMinorVersion > aMinorVersion) return YES;
-
-	NSString *serverReleasePart = [serverVersionParts objectAtIndex:2];
-	NSUInteger serverReleaseVersion = (NSUInteger)[[[serverReleasePart componentsSeparatedByString:@"-"] objectAtIndex:0] integerValue];
-	if (serverReleaseVersion < aReleaseVersion) return NO;
-	return YES;
+	return (myver >= serverVersionNumber);
 }
 
 #pragma mark -

Frameworks/SPMySQLFramework/Source/SPMySQLConnection Categories/Ping & KeepAlive.m
@@ -94,15 +94,15 @@
 		// attempt a single reconnection in the background
 		if (_elapsedSecondsSinceAbsoluteTime(lastConnectionUsedTime) < 60 * 15) {
 			[self _reconnectAfterBackgroundConnectionLoss];
-
+		}
 		// Otherwise set the state to connection lost for automatic reconnect on
 		// next use.
-		} else {
+		else {
 			state = SPMySQLConnectionLostInBackground;
 		}
 
 		// Return as no further ping action required this cycle.
-		goto end;
+		goto end_cleanup;
 	}
 
 	// Otherwise, perform a background ping.
@@ -112,7 +112,7 @@
 	} else {
 		keepAlivePingFailures++;
 	}
-end:
+end_cleanup:
 	keepAliveThread = nil;
 }
 
@@ -139,6 +139,11 @@ end:
 
 	// Set up a query lock
 	[self _lockConnection];
+	//we might find ourselves at the losing end of a contest with -[self _disconnect]
+	if(!mySQLConnection) {
+		[self _unlockConnection];
+		return NO;
+	}
 
 	volatile BOOL keepAliveLastPingSuccess = NO;
 	keepAliveLastPingBlocked = NO;

Frameworks/SPMySQLFramework/Source/SPMySQLConnection.h
@@ -99,6 +99,7 @@
 
 	// Server details
 	NSString *serverVariableVersion;
+	unsigned long serverVersionNumber;
 
 	// Error state for the last query or connection state
 	NSUInteger queryErrorID;

Frameworks/SPMySQLFramework/Source/SPMySQLConnection.m
@@ -174,6 +174,7 @@ const char *SPMySQLSSLPermissibleCiphers = "DHE-RSA-AES256-SHA:AES256-SHA:DHE-RS
 
 		// Ensure the server detail records are initialised
 		serverVariableVersion = nil;
+		serverVersionNumber = 0;
 
 		// Start with a blank error state
 		queryErrorID = 0;
@@ -474,6 +475,19 @@ asm(".desc ___crashreporter_info__, 0x10");
 	initialConnectTime = mach_absolute_time();
 	mysqlConnectionThreadId = mySQLConnection->thread_id;
 	lastConnectionUsedTime = initialConnectTime;
+
+	// Copy the server version string to the instance variable
+	if (serverVariableVersion) [serverVariableVersion release], serverVariableVersion = nil;
+	// the mysql_get_server_info() function
+	//   * returns the version name that is part of the initial connection handshake.
+	//   * Unless the connection failed, it will always return a non-null buffer containing at least a '\0'.
+	//   * It will never affect the error variables (since it only returns a struct member)
+	//
+	// At that point (handshake) there is no charset and it's highly unlikely this will ever contain something other than ASCII,
+	// but to be safe, we'll use the Latin1 encoding which won't bail on invalid chars.
+	serverVariableVersion = [[NSString alloc] initWithCString:mysql_get_server_info(mySQLConnection) encoding:NSISOLatin1StringEncoding];
+	// this one can actually change the error state, but only if the server version string is not set (ie. no connection)
+	serverVersionNumber = mysql_get_server_version(mySQLConnection);
 
 	// Update SSL state
 	connectedWithSSL = NO;
@@ -892,6 +906,7 @@ asm(".desc ___crashreporter_info__, 0x10");
 	[self _unlockConnection];
 	[self _cancelKeepAlives];
 
+	[self _lockConnection];
 	// Close the underlying MySQL connection if it still appears to be active, and not reading
 	// or writing.  While this may result in a leak of the MySQL object, it prevents crashes
 	// due to attempts to close a blocked/stuck connection.
@@ -899,17 +914,16 @@ asm(".desc ___crashreporter_info__, 0x10");
 		mysql_close(mySQLConnection);
 	}
 	mySQLConnection = NULL;
+	if (serverVariableVersion) [serverVariableVersion release], serverVariableVersion = nil;
+	serverVersionNumber = 0;
+	if (database) [database release], database = nil;
+	state = SPMySQLDisconnected;
+	[self _unlockConnection];
 
 	// If using a connection proxy, disconnect that too
 	if (proxy) {
 		[proxy performSelectorOnMainThread:@selector(disconnect) withObject:nil waitUntilDone:YES];
 	}
-
-	// Clear host-specific information
-	if (serverVariableVersion) [serverVariableVersion release], serverVariableVersion = nil;
-	if (database) [database release], database = nil;
-
-	state = SPMySQLDisconnected;
 }
 
 /**
@@ -933,10 +947,6 @@ asm(".desc ___crashreporter_info__, 0x10");
 	for (NSArray *variableRow in theResult) {
 		[variables setObject:[variableRow objectAtIndex:1] forKey:[variableRow objectAtIndex:0]];
 	}
-
-	// Copy the server version string to the instance variable
-	if (serverVariableVersion) [serverVariableVersion release], serverVariableVersion = nil;
-	serverVariableVersion = [[variables objectForKey:@"version"] retain];
 
 	// Get the connection encoding.  Although a specific encoding may have been requested on
 	// connection, it may be overridden by init_connect commands or connection state changes.

Source/SPFieldMapperController.m
@@ -1043,9 +1043,7 @@ static NSUInteger SPSourceColumnTypeInteger     = 1;
 	NSArray *encodings  = [databaseDataInstance getDatabaseCharacterSetEncodings];
 	NSString *utf8MenuItemTitle = nil;
 	
-	if ([encodings count] > 0
-		&& ([mySQLConnection serverMajorVersion] > 4
-			|| ([mySQLConnection serverMajorVersion] == 4 && [mySQLConnection serverMinorVersion] >= 1)))
+	if ([encodings count] > 0 && ([mySQLConnection serverVersionIsGreaterThanOrEqualTo:4 minorVersion:1 releaseVersion:0]))
 	{
 		[[newTableInfoEncodingPopup menu] addItem:[NSMenuItem separatorItem]];
 		for (NSDictionary *encoding in encodings) {

Source/SPServerSupport.m
@@ -95,10 +95,9 @@
 - (id)initWithMajorVersion:(NSInteger)majorVersion minor:(NSInteger)minorVersion release:(NSInteger)releaseVersion
 {
 	if ((self = [super init])) {
-		// Might be NSNotFound if unknown. unknown should also lose against "0".
-		serverMajorVersion   = (majorVersion   != NSNotFound)? majorVersion   : -1;
-		serverMinorVersion   = (minorVersion   != NSNotFound)? minorVersion   : -1;
-		serverReleaseVersion = (releaseVersion != NSNotFound)? releaseVersion : -1;
+		serverMajorVersion   = majorVersion;
+		serverMinorVersion   = minorVersion;
+		serverReleaseVersion = releaseVersion;
 		
 		// Determine what the server supports
 		[self evaluate];
@@ -363,9 +362,9 @@
 - (void)dealloc
 {
 	// Reset version integers
-	serverMajorVersion   = -1;
-	serverMinorVersion   = -1;
-	serverReleaseVersion = -1;
+	serverMajorVersion   = 0;
+	serverMinorVersion   = 0;
+	serverReleaseVersion = 0;
 	
 	// Invalidate all ivars
 	[self _invalidate];

commit 2f2aafb4f5675282a37d16dce96027706096df40

* Basic math is hard sometimes (fixes an issue introduced by me in b2d798ba9282d3acf1a2d65de30849e529d4d255)
* Fix an exception that could occur when trying to view a damaged table
* Fix a theoretical use-after-free issue by a wrongly structured retain/release in a setter

Frameworks/SPMySQLFramework/Source/SPMySQLConnection Categories/Server Info.m
@@ -87,7 +87,7 @@
 {
 	unsigned long myver = aMajorVersion * 10000 + aMinorVersion * 100 + aReleaseVersion;
 
-	return (myver >= serverVersionNumber);
+	return (serverVersionNumber >= myver);
 }
 
 #pragma mark -

Source/SPDatabaseData.m
@@ -471,6 +471,9 @@ copy_return:
 	
 	[result setReturnDataAsStrings:YES];
 	
+	if([connection queryErrored])
+		SPLog(@"server variable lookup failed for '%@': %@ (%lu)",variable,[connection lastErrorMessage],[connection lastErrorID]);
+	
 	if ([result numberOfRows] != 1)
 		return nil;
 	

Source/SPDatabaseDocument.m
@@ -1729,20 +1729,20 @@ static int64_t SPDatabaseDocumentInstanceCounter = 0;
 {
 	_supportsEncoding = YES;
 
-	NSString *mysqlEncoding = [databaseDataInstance getDatabaseDefaultCharacterSet];
+	NSString *mysqlEncoding = [[databaseDataInstance getDatabaseDefaultCharacterSet] retain];
 
 	SPClear(selectedDatabaseEncoding);
 
 	// Fallback or older version? -> set encoding to mysql default encoding latin1
 	if ( !mysqlEncoding ) {
-		NSLog(@"Error: no character encoding found, mysql version is %@", [self mySQLVersion]);
+		NSLog(@"Error: no character encoding found for db, mysql version is %@", [self mySQLVersion]);
 		
 		selectedDatabaseEncoding = [[NSString alloc] initWithString:@"latin1"];
 		
 		_supportsEncoding = NO;
 	} 
 	else {
-		selectedDatabaseEncoding = [mysqlEncoding retain];
+		selectedDatabaseEncoding = mysqlEncoding;
 	}
 }
 

Source/SPTableContent.m
@@ -434,7 +434,8 @@ static NSString *SPTableFilterSetDefaultOperator = @"SPTableFilterSetDefaultOper
 		[tableContentView scrollColumnToVisible:0];
 
 		// Set the maximum table rows to an estimated count pre-load
-		maxNumRows = [[tableDataInstance statusValueForKey:@"Rows"] integerValue];
+		NSString *rows = [tableDataInstance statusValueForKey:@"Rows"];
+		maxNumRows = (rows && ![rows isNSNull])? [rows integerValue] : 0;
 		maxNumRowsIsEstimate = YES;
 	}
 	
@@ -3991,14 +3992,15 @@ static NSString *SPTableFilterSetDefaultOperator = @"SPTableFilterSetDefaultOper
 		[tableDataInstance updateAccurateNumberOfRowsForCurrentTableForcingUpdate:NO];
 
 		// If the state is now accurate, use it
+		NSString *rows = [tableDataInstance statusValueForKey:@"Rows"];
 		if ([[tableDataInstance statusValueForKey:@"RowsCountAccurate"] boolValue]) {
-			maxNumRows = [[tableDataInstance statusValueForKey:@"Rows"] integerValue];
+			maxNumRows = [rows integerValue];
 			maxNumRowsIsEstimate = NO;
 			checkStatusCount = YES;
-
-			// Otherwise, use the estimate count
-		} else {
-			maxNumRows = [[tableDataInstance statusValueForKey:@"Rows"] integerValue];
+		}
+		// Otherwise, use the estimate count
+		else {
+			maxNumRows = (rows && ![rows isNSNull])? [rows integerValue] : 0;
 			maxNumRowsIsEstimate = YES;
 			checkStatusCount = YES;
 		}

Source/SPTableData.m
@@ -1004,7 +1004,7 @@
 			SPOnewayAlertSheet(
 				NSLocalizedString(@"Error", @"error"),
 				[NSApp mainWindow],
-				[NSString stringWithFormat:NSLocalizedString(@"An error occured while retrieving status data.\nMySQL said: %@", @"message of panel when retrieving view information failed"), [mySQLConnection lastErrorMessage]]
+				[NSString stringWithFormat:NSLocalizedString(@"An error occured while retrieving status data.\n\nMySQL said: %@", @"message of panel when retrieving view information failed"), [mySQLConnection lastErrorMessage]]
 			);
 			if (changeEncoding) [mySQLConnection restoreStoredEncoding];
 		}
@@ -1041,9 +1041,19 @@
 		// this happens e.g. for db "information_schema"
 		if([[status objectForKey:@"Rows"] isNSNull]) {
 			tableStatusResult = [mySQLConnection queryString:[NSString stringWithFormat:@"SELECT COUNT(1) FROM %@", [escapedTableName backtickQuotedString] ]];
-			if (![mySQLConnection queryErrored])
+			// this query can fail e.g. if a table is damaged
+			if (tableStatusResult && ![mySQLConnection queryErrored]) {
 				[status setObject:[[tableStatusResult getRowAsArray] objectAtIndex:0] forKey:@"Rows"];
 				[status setObject:@"y" forKey:@"RowsCountAccurate"];
+			}
+			else {
+				//FIXME that error should really show only when trying to view the table content, but we don't even try to load that if Rows==NULL
+				SPOnewayAlertSheet(
+					NSLocalizedString(@"Querying row count failed", @"table status : row count query failed : error title"),
+					[NSApp mainWindow],
+					[NSString stringWithFormat:NSLocalizedString(@"An error occured while trying to determine the number of rows for “%@”.\nMySQL said: %@ (%lu)", @"table status : row count query failed : error message"),[tableListInstance tableName],[mySQLConnection lastErrorMessage],[mySQLConnection lastErrorID]]
+				);
+			}
 		}
 
 	}