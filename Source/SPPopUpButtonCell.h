sequelpro/sequelpro

commit f02fb787063caabe246a0ee420394f5676c55a9c

Change the internal handling of charset/collation from offset-based to name-based in structure view
This is a major change to the charset/collation code. Please watch out for issues!
(part of #2237)

Source/SPTableStructureDelegate.m
@@ -37,6 +37,7 @@
 #import "SPTableFieldValidation.h"
 #import "SPTableStructureLoading.h"
 #import "SPServerSupport.h"
+#import "SPTablesList.h"
 
 #import <SPMySQL/SPMySQL.h>
 
@@ -60,72 +61,68 @@
 {
 	// Return a placeholder if the table is reloading
 	if ((NSUInteger)rowIndex >= [tableFields count]) return @"...";
+
+	NSDictionary *rowData = NSArrayObjectAtIndex(tableFields, rowIndex);
 	
 	if ([[tableColumn identifier] isEqualToString:@"collation"]) {
 		NSString *tableEncoding = [tableDataInstance tableEncoding];
-		NSString *columnEncoding = nil;
-		NSInteger idx = 0;
-		
-		if ((idx = [[NSArrayObjectAtIndex(tableFields, rowIndex) objectForKey:@"encoding"] integerValue]) > 0 && idx < [encodingPopupCell numberOfItems]) {
-			NSString *enc = [[encodingPopupCell itemAtIndex:idx] title];
+		NSString *columnEncoding = [rowData objectForKey:@"encodingName"];
+		NSString *columnCollation = [rowData objectForKey:@"collationName"]; // loadTable: has already inferred it, if not set explicit
 
-			NSUInteger start = [enc rangeOfString:@"("].location + 1;
-			
-			columnEncoding = [enc substringWithRange:NSMakeRange(start, [enc length] - start - 1)];
-			collations = [databaseDataInstance getDatabaseCollationsForEncoding:columnEncoding];
-		} 
-		else {
-			// If the structure has loaded (not still loading!) and the table encoding
-			// is set, use the appropriate collations.
-			collations = @[];
-			if([tableDocumentInstance structureLoaded]) {
-				columnEncoding = [tableDataInstance tableEncoding];
-				if(columnEncoding) collations = [databaseDataInstance getDatabaseCollationsForEncoding:columnEncoding];
-			}
-		}
-		
 		NSPopUpButtonCell *collationCell = [tableColumn dataCell];
-		
 		[collationCell removeAllItems];
-		
-		if ([collations count] > 0) {
-			NSString *tableCollation = [[tableDataInstance statusValues] objectForKey:@"Collation"];
-			
-			if (![tableCollation length]) {
-				tableCollation = [databaseDataInstance getDefaultCollationForEncoding:tableEncoding];
-			}
-			
-			NSString *columnCollation = [NSArrayObjectAtIndex(tableFields, rowIndex) objectForKey:@"collationName"];
-			
-			if (![columnCollation length]) {
-				columnCollation = [databaseDataInstance getDefaultCollationForEncoding:columnEncoding];
-			}
-			
-			[[tableColumn dataCell] addItemWithTitle:@""];
+		[collationCell addItemWithTitle:@"dummy"];
+		//copy the default style of menu items and add gray color for default item
+		NSMutableDictionary *menuAttrs = [NSMutableDictionary dictionaryWithDictionary:[[collationCell attributedTitle] attributesAtIndex:0 effectiveRange:NULL]];
+		[menuAttrs setObject:[NSColor lightGrayColor] forKey:NSForegroundColorAttributeName];
+		[[collationCell lastItem] setTitle:@""];
 
-			BOOL useMonospacedFont = [prefs boolForKey:SPUseMonospacedFonts];
-			CGFloat monospacedFontSize = [prefs floatForKey:SPMonospacedFontSize] > 0 ? [prefs floatForKey:SPMonospacedFontSize] : [NSFont smallSystemFontSize];
-			NSMutableDictionary *menuAttributes = [NSMutableDictionary dictionaryWithObject:[NSColor lightGrayColor] forKey:NSForegroundColorAttributeName];
-			[menuAttributes setObject:useMonospacedFont ? [NSFont fontWithName:SPDefaultMonospacedFontName size:monospacedFontSize] : [NSFont systemFontOfSize:[NSFont smallSystemFontSize]] forKey:NSFontAttributeName];
-			
-			BOOL columnUsesTableDefaultEncoding = ([columnEncoding isEqualToString:tableEncoding]);
-			// Populate collation popup button
-			for (NSDictionary *collation in collations)
-			{
-				NSString *collationName = [collation objectForKey:@"COLLATION_NAME"];
-				
-				[[tableColumn dataCell] addItemWithTitle:collationName];
+		//if this is not set the column either has no encoding (numeric etc.) or retrieval failed. Either way we can't provide collations
+		if(columnEncoding) {
+			collations = [databaseDataInstance getDatabaseCollationsForEncoding:columnEncoding];
 
-				// If this matches the table's collation, draw in gray
-				if (columnUsesTableDefaultEncoding && [collationName isEqualToString:tableCollation]) {
-					NSMenuItem *collationMenuItem = [(NSPopUpButtonCell *)[tableColumn dataCell] itemAtIndex:([[tableColumn dataCell] numberOfItems] - 1)];
+			if ([collations count] > 0) {
+				NSString *tableCollation = [[tableDataInstance statusValues] objectForKey:@"Collation"];
 
-					NSAttributedString *itemString = [[[NSAttributedString alloc] initWithString:collationName attributes:menuAttributes] autorelease];
-					
-					[collationMenuItem setAttributedTitle:itemString];
+				if (![tableCollation length]) {
+					tableCollation = [databaseDataInstance getDefaultCollationForEncoding:tableEncoding];
 				}
+
+				BOOL columnUsesTableDefaultEncoding = ([columnEncoding isEqualToString:tableEncoding]);
+				// Populate collation popup button
+				for (NSDictionary *collation in collations)
+				{
+					NSString *collationName = [collation objectForKey:@"COLLATION_NAME"];
+
+					[collationCell addItemWithTitle:collationName];
+					NSMenuItem *item = [collationCell lastItem];
+					[item setRepresentedObject:collationName];
+
+					// If this matches the table's collation, draw in gray
+					if (columnUsesTableDefaultEncoding && [collationName isEqualToString:tableCollation]) {
+						NSAttributedString *itemString = [[NSAttributedString alloc] initWithString:[item title] attributes:menuAttrs];
+						[item setAttributedTitle:[itemString autorelease]];
+					}
+				}
+
+				//look up the right item
+				NSInteger idx = [collationCell indexOfItemWithRepresentedObject:columnCollation];
+				if(idx > 0) return @(idx);
 			}
 		}
+
+		return @0;
+	}
+	else if ([[tableColumn identifier] isEqualToString:@"encoding"]) {
+		// the encoding menu was already configured during setTableDetails:
+		NSString *columnEncoding = [rowData objectForKey:@"encodingName"];
+
+		if(columnEncoding) {
+			NSInteger idx = [encodingPopupCell indexOfItemWithRepresentedObject:columnEncoding];
+			if(idx > 0) return @(idx);
+		}
+
+		return @0;
 	}
 	else if ([[tableColumn identifier] isEqualToString:@"Extra"]) {
 		id dataCell = [tableColumn dataCell];
@@ -141,7 +138,7 @@
 		}
 	}
 
-	return [NSArrayObjectAtIndex(tableFields, rowIndex) objectForKey:[tableColumn identifier]];
+	return [rowData objectForKey:[tableColumn identifier]];
 }
 
 - (void)tableView:(NSTableView *)aTableView setObjectValue:(id)anObject forTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex
@@ -159,16 +156,32 @@
 
 	// Reset collation if encoding was changed
 	if ([[aTableColumn identifier] isEqualToString:@"encoding"]) {
-		if ([[currentRow objectForKey:@"encoding"] integerValue] != [anObject integerValue]) {
-			[currentRow setObject:@0 forKey:@"collation"];
+		NSString *oldEncoding = [currentRow objectForKey:@"encodingName"];
+		NSString *newEncoding = [[encodingPopupCell itemAtIndex:[anObject integerValue]] representedObject];
+		if (![oldEncoding isEqualToString:newEncoding]) {
+			[currentRow removeObjectForKey:@"collationName"];
 			[tableSourceView reloadData];
 		}
+		if(!newEncoding)
+			[currentRow removeObjectForKey:@"encodingName"];
+		else
+			[currentRow setObject:newEncoding forKey:@"encodingName"];
+		return;
+	}
+	else if ([[aTableColumn identifier] isEqualToString:@"collation"]) {
+		NSString *newCollation = [[(NSPopUpButtonCell *)[aTableColumn dataCell] itemAtIndex:[anObject integerValue]] representedObject];
+
+		if(!newCollation)
+			[currentRow removeObjectForKey:@"collationName"];
+		else
+			[currentRow setObject:newCollation forKey:@"collationName"];
+		return;
 	}
 	// Reset collation if BINARY was set changed, as enabling BINARY sets collation to *_bin
 	else if ([[aTableColumn identifier] isEqualToString:@"binary"]) {
 		if ([[currentRow objectForKey:@"binary"] integerValue] != [anObject integerValue]) {
-			[currentRow setObject:@0 forKey:@"collation"];
-			
+			[currentRow removeObjectForKey:@"collationName"];
+
 			[tableSourceView reloadData];
 		}
 	}
@@ -220,9 +233,8 @@
 			[tableSourceView reloadData];
 		}
 	}
-	
 	// Store new value but not if user choose "---" for type and reset values if required
-	if ([[aTableColumn identifier] isEqualToString:@"type"]) {
+	else if ([[aTableColumn identifier] isEqualToString:@"type"]) {
 		if (anObject && [(NSString*)anObject length] && ![(NSString*)anObject hasPrefix:@"--"]) {
 			[currentRow setObject:[(NSString*)anObject uppercaseString] forKey:@"type"];
 			
@@ -238,10 +250,10 @@
 			
 			[tableSourceView reloadData];
 		}
+		return;
 	} 
-	else {
-		[currentRow setObject:(anObject) ? anObject : @"" forKey:[aTableColumn identifier]];
-	}
+
+	[currentRow setObject:(anObject) ? anObject : @"" forKey:[aTableColumn identifier]];
 }
 
 /**
@@ -270,7 +282,7 @@
 	
 	if ([rows count] == 1) {
 		[pboard declareTypes:@[SPDefaultPasteboardDragType] owner:nil];
-		[pboard setString:[[NSNumber numberWithInteger:[rows firstIndex]] stringValue] forType:SPDefaultPasteboardDragType];
+		[pboard setString:[NSString stringWithFormat:@"%lu",[rows firstIndex]] forType:SPDefaultPasteboardDragType];
 		
 		return YES;
 	} 
@@ -332,27 +344,19 @@
 		[queryString appendFormat:@"(%@)", [originalRow objectForKey:@"length"]];
 	}
 	
-	NSString *fieldEncoding = @"";
-			
-	if ([[originalRow objectForKey:@"encoding"] integerValue] > 0 && [[tableDocumentInstance serverSupport] supportsPost41CharacterSetHandling]) {
-		NSString *enc = [[encodingPopupCell itemAtIndex:[[originalRow objectForKey:@"encoding"] integerValue]] title];
-		
-		NSInteger start = [enc rangeOfString:@"("].location + 1;
-		
-		fieldEncoding = [enc substringWithRange:NSMakeRange(start, [enc length] - start - 1)];
-		
+	NSString *fieldEncoding = [originalRow objectForKey:@"encodingName"];
+
+	if ([fieldEncoding length] && [[tableDocumentInstance serverSupport] supportsPost41CharacterSetHandling]) {
 		[queryString appendFormat:@" CHARACTER SET %@", fieldEncoding];
 	}
 	
 	if (![fieldEncoding length] && [tableDataInstance tableEncoding]) {
 		fieldEncoding = [tableDataInstance tableEncoding];
 	}
-	
-	if ([fieldEncoding length] && [[originalRow objectForKey:@"collation"] integerValue] > 0 && ![[originalRow objectForKey:@"binary"] integerValue]) {
-		NSArray *theCollations = [databaseDataInstance getDatabaseCollationsForEncoding:fieldEncoding];
-		NSString *col = [[theCollations objectAtIndex:[[originalRow objectForKey:@"collation"] integerValue] - 1] objectForKey:@"COLLATION_NAME"];
-		
-		[queryString appendFormat:@" COLLATE %@", col];
+
+	NSString *fieldCollation = [originalRow objectForKey:@"collationName"];
+	if ([fieldEncoding length] && [fieldCollation length] && ![[originalRow objectForKey:@"binary"] integerValue]) {
+		[queryString appendFormat:@" COLLATE %@", fieldCollation];
 	}
 
 	// Add unsigned, zerofill, binary, not null if necessary

Source/SPTableStructure.m
@@ -46,6 +46,7 @@
 #import "SPTableStructureLoading.h"
 #import "SPThreadAdditions.h"
 #import "SPServerSupport.h"
+#import "SPExtendedTableInfo.h"
 
 #import <SPMySQL/SPMySQL.h>
 
@@ -252,8 +253,8 @@ static NSString *SPRemoveFieldAndForeignKey = @"SPRemoveFieldAndForeignKey";
 	BOOL allowNull = [[[tableDataInstance statusValueForKey:@"Engine"] uppercaseString] isEqualToString:@"CSV"] ? NO : [prefs boolForKey:SPNewFieldsAllowNulls];
 	
 	[tableFields insertObject:[NSMutableDictionary
-							   dictionaryWithObjects:[NSArray arrayWithObjects:@"", @"INT", @"", @"0", @"0", @"0", allowNull ? @"1" : @"0", @"", [prefs stringForKey:SPNullValue], @"None", @"", @0, @0, nil]
-							   forKeys:@[@"name", @"type", @"length", @"unsigned", @"zerofill", @"binary", @"null", @"Key", @"default", @"Extra", @"comment", @"encoding", @"collation"]]
+							   dictionaryWithObjects:[NSArray arrayWithObjects:@"", @"INT", @"", @"0", @"0", @"0", allowNull ? @"1" : @"0", @"", [prefs stringForKey:SPNullValue], @"None", @"", nil]
+							   forKeys:@[@"name", @"type", @"length", @"unsigned", @"zerofill", @"binary", @"null", @"Key", @"default", @"Extra", @"comment"]]
 					  atIndex:insertIndex];
 #else
 	[tableFields insertObject:[NSMutableDictionary
@@ -810,29 +811,24 @@ static NSString *SPRemoveFieldAndForeignKey = @"SPRemoveFieldAndForeignKey";
 
 
 		if ([fieldValidation isFieldTypeString:theRowType]) {
+			BOOL charsetSupport = [[tableDocumentInstance serverSupport] supportsPost41CharacterSetHandling];
+
 			// Add CHARSET
-			NSString *fieldEncoding = @"";
-			if([[theRow objectForKey:@"encoding"] integerValue] > 0 && [[tableDocumentInstance serverSupport] supportsPost41CharacterSetHandling]) {
-				NSString *enc = [[encodingPopupCell itemAtIndex:[[theRow objectForKey:@"encoding"] integerValue]] title];
-				NSInteger start = [enc rangeOfString:@"("].location+1;
-				NSInteger end = [enc length] - start - 1;
-				fieldEncoding = [enc substringWithRange:NSMakeRange(start, end)];
+			NSString *fieldEncoding = [theRow objectForKey:@"encodingName"];
+			if(charsetSupport && [fieldEncoding length]) {
 				[queryString appendFormat:@"\n CHARACTER SET %@", fieldEncoding];
 			}
-			// Remember CHARSET for COLLATE
-			if(![fieldEncoding length] && [tableDataInstance tableEncoding]) {
-				fieldEncoding = [tableDataInstance tableEncoding];
-			}
 
-			// ADD COLLATE
-			if([fieldEncoding length] && [[theRow objectForKey:@"collation"] integerValue] > 0 && ![[theRow objectForKey:@"binary"] integerValue]) {
-				NSArray *theCollations = [databaseDataInstance getDatabaseCollationsForEncoding:fieldEncoding];
-				NSString *col = [[theCollations objectAtIndex:[[theRow objectForKey:@"collation"] integerValue]-1] objectForKey:@"COLLATION_NAME"];
-				[queryString appendFormat:@"\n COLLATE %@", col];
-			}
-
-			if ( [[theRow objectForKey:@"binary"] integerValue] == 1) {
+			if ([[theRow objectForKey:@"binary"] integerValue] == 1) {
 				[queryString appendString:@"\n BINARY"];
+			}
+			else {
+				// ADD COLLATE
+				// Note: a collate without charset is valid in MySQL. The charset can be determined from a collation.
+				NSString *fieldCollation = [theRow objectForKey:@"collationName"];
+				if(charsetSupport && [fieldCollation length]) {
+					[queryString appendFormat:@"\n COLLATE %@", fieldCollation];
+				}
 			}
 
 		}

commit 74a90251228a07995ea5ab1bae0fb2428f9cfbc7

Fix an issue where changing a table collation could cause an exception (fixes #2320)
This issue probably was introduced in f02fb787063caabe246a0ee420394f5676c55a9c

The empty item at the top of the collation list will now no longer have a selection mark, though.

Interfaces/English.lproj/DBView.xib
@@ -26580,6 +26580,7 @@ AAEAAQAAAT0AAwAAAAEAAgAAAVIAAwAAAAEAAQAAAVMAAwAAAAIAAQABAAAAAA</bytes>
 				</object>
 				<string key="7491.IBPluginDependency">com.apple.InterfaceBuilder.CocoaPlugin</string>
 				<boolean value="NO" key="7491.showNotes"/>
+				<string key="7493.CustomClassName">SPPopUpButtonCell</string>
 				<string key="7493.IBPluginDependency">com.apple.InterfaceBuilder.CocoaPlugin</string>
 				<boolean value="NO" key="7493.showNotes"/>
 				<string key="7494.CustomClassName">SPIdMenu</string>
@@ -30319,6 +30320,14 @@ AAEAAQAAAT0AAwAAAAEAAgAAAVIAAwAAAAEAAQAAAVMAAwAAAAIAAQABAAAAAA</bytes>
 					<object class="IBClassDescriptionSource" key="sourceIdentifier">
 						<string key="majorKey">IBProjectSource</string>
 						<string key="minorKey">../Source/SPIndexesController.m</string>
+					</object>
+				</object>
+				<object class="IBPartialClassDescription">
+					<string key="className">SPPopUpButtonCell</string>
+					<string key="superclassName">NSPopUpButtonCell</string>
+					<object class="IBClassDescriptionSource" key="sourceIdentifier">
+						<string key="majorKey">IBProjectSource</string>
+						<string key="minorKey">../Source/SPPopUpButtonCell.h</string>
 					</object>
 				</object>
 				<object class="IBPartialClassDescription">

Source/SPPopUpButtonCell.h
@@ -1,0 +1,48 @@
+//
+//  SPPopUpButtonCell.h
+//  sequel-pro
+//
+//  Created by Max Lohrmann on 10.11.15.
+//  Copyright (c) 2015 Max Lohrmann. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+//
+//  More info at <https://github.com/sequelpro/sequelpro>
+
+#import <Cocoa/Cocoa.h>
+
+/**
+ * The only difference here is that objectValue/setObjectValue:
+ * will use the representedObject of the menu instead of the menu item's index
+ *
+ * nil will act the same as @(-1) would with the regular NSPopUpButtonCell.
+ *
+ * Note that in theory multiple menu items could have the same representedObject
+ * and identification is done via isEqual: so this class would only ever
+ * pick the first one.
+ */
+@interface SPPopUpButtonCell : NSPopUpButtonCell
+
+- (id)objectValue;
+- (void)setObjectValue:(id)objectValue;
+
+@end

Source/SPPopUpButtonCell.m
@@ -1,0 +1,51 @@
+//
+//  SPPopUpButtonCell.m
+//  sequel-pro
+//
+//  Created by Max Lohrmann on 10.11.15.
+//  Copyright (c) 2015 Max Lohrmann. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+//
+//  More info at <https://github.com/sequelpro/sequelpro>
+
+#import "SPPopUpButtonCell.h"
+
+@implementation SPPopUpButtonCell
+
+- (id)objectValue
+{
+	NSInteger n = [[super objectValue] integerValue];
+	
+	// this method can be called for invalid selections, which return -1, which fails with itemAtIndex:
+	if(n < 0 || n >= [self numberOfItems]) return nil;
+	
+	return [[self itemAtIndex:n] representedObject];
+}
+
+- (void)setObjectValue:(id)objectValue
+{
+	NSInteger n = [self indexOfItemWithRepresentedObject:objectValue];
+	[super setObjectValue:@(n)];
+}
+
+@end

Source/SPTableStructureDelegate.m
@@ -102,7 +102,7 @@ static void _BuildMenuWithPills(NSMenu *menu,struct _cmpMap *map,size_t mapEntri
 		[[collationCell lastItem] setTitle:@""];
 
 		//if this is not set the column either has no encoding (numeric etc.) or retrieval failed. Either way we can't provide collations
-		if(columnEncoding) {
+		if([columnEncoding length]) {
 			collations = [databaseDataInstance getDatabaseCollationsForEncoding:columnEncoding];
 
 			if ([collations count] > 0) {
@@ -129,19 +129,18 @@ static void _BuildMenuWithPills(NSMenu *menu,struct _cmpMap *map,size_t mapEntri
 					}
 				}
 
-				//look up the right item
-				NSInteger idx = [collationCell indexOfItemWithRepresentedObject:columnCollation];
-				if(idx > 0) return @(idx);
+				// the popup cell is subclassed to take the representedObject instead of the item index
+				return columnCollation;
 			}
 		}
 
-		return @0;
+		return nil;
 	}
 	else if ([[tableColumn identifier] isEqualToString:@"encoding"]) {
 		// the encoding menu was already configured during setTableDetails:
 		NSString *columnEncoding = [rowData objectForKey:@"encodingName"];
 
-		if(columnEncoding) {
+		if([columnEncoding length]) {
 			NSInteger idx = [encodingPopupCell indexOfItemWithRepresentedObject:columnEncoding];
 			if(idx > 0) return @(idx);
 		}
@@ -193,7 +192,8 @@ static void _BuildMenuWithPills(NSMenu *menu,struct _cmpMap *map,size_t mapEntri
 		return;
 	}
 	else if ([[aTableColumn identifier] isEqualToString:@"collation"]) {
-		NSString *newCollation = [[(NSPopUpButtonCell *)[aTableColumn dataCell] itemAtIndex:[anObject integerValue]] representedObject];
+		//the popup button is subclassed to return the representedObject instead of the item index
+		NSString *newCollation = anObject;
 
 		if(!newCollation)
 			[currentRow removeObjectForKey:@"collationName"];

sequel-pro.xcodeproj/project.pbxproj
@@ -200,6 +200,7 @@
 		507FF26A1BC8450100104523 /* SPExportSettingsPersistence.m in Sources */ = {isa = PBXBuildFile; fileRef = 507FF2691BC8450100104523 /* SPExportSettingsPersistence.m */; };
 		507FF2A11BCD27A700104523 /* SPFunctions.m in Sources */ = {isa = PBXBuildFile; fileRef = 507FF1111BBCC57600104523 /* SPFunctions.m */; };
 		507FF2A21BCD27AE00104523 /* SPOSInfo.m in Sources */ = {isa = PBXBuildFile; fileRef = 50EAB5B71A8FBB08008F627A /* SPOSInfo.m */; };
+		50805B0D1BF2A068005F7A99 /* SPPopUpButtonCell.m in Sources */ = {isa = PBXBuildFile; fileRef = 50805B0C1BF2A068005F7A99 /* SPPopUpButtonCell.m */; };
 		5089B0271BE714E300E226CD /* SPIdMenu.m in Sources */ = {isa = PBXBuildFile; fileRef = 5089B0261BE714E300E226CD /* SPIdMenu.m */; };
 		50A9F8B119EAD4B90053E571 /* SPGotoDatabaseController.m in Sources */ = {isa = PBXBuildFile; fileRef = 50A9F8B019EAD4B90053E571 /* SPGotoDatabaseController.m */; };
 		50D3C3491A75B8A800B5429C /* GotoDatabaseDialog.xib in Resources */ = {isa = PBXBuildFile; fileRef = 50D3C34B1A75B8A800B5429C /* GotoDatabaseDialog.xib */; };
@@ -923,6 +924,8 @@
 		507FF1111BBCC57600104523 /* SPFunctions.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = SPFunctions.m; sourceTree = "<group>"; };
 		507FF2681BC8450100104523 /* SPExportSettingsPersistence.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = SPExportSettingsPersistence.h; sourceTree = "<group>"; };
 		507FF2691BC8450100104523 /* SPExportSettingsPersistence.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = SPExportSettingsPersistence.m; sourceTree = "<group>"; };
+		50805B0B1BF2A068005F7A99 /* SPPopUpButtonCell.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = SPPopUpButtonCell.h; sourceTree = "<group>"; };
+		50805B0C1BF2A068005F7A99 /* SPPopUpButtonCell.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = SPPopUpButtonCell.m; sourceTree = "<group>"; };
 		5089B0251BE714E300E226CD /* SPIdMenu.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = SPIdMenu.h; sourceTree = "<group>"; };
 		5089B0261BE714E300E226CD /* SPIdMenu.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = SPIdMenu.m; sourceTree = "<group>"; };
 		50A9F8AF19EAD4B90053E571 /* SPGotoDatabaseController.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = SPGotoDatabaseController.h; sourceTree = "<group>"; };
@@ -2326,6 +2329,8 @@
 				17FDB04B1280778B00DBBBC2 /* SPFontPreviewTextField.m */,
 				58D2A6A516FBDEFF002EB401 /* SPComboPopupButton.h */,
 				58D2A6A616FBDEFF002EB401 /* SPComboPopupButton.m */,
+				50805B0B1BF2A068005F7A99 /* SPPopUpButtonCell.h */,
+				50805B0C1BF2A068005F7A99 /* SPPopUpButtonCell.m */,
 			);
 			name = Controls;
 			sourceTree = "<group>";
@@ -3286,6 +3291,7 @@
 				173C837A11AAD2AE00B8B084 /* SPHTMLExporter.m in Sources */,
 				173C837B11AAD2AE00B8B084 /* SPPDFExporter.m in Sources */,
 				173C839011AAD32A00B8B084 /* SPCSVExporterDelegate.m in Sources */,
+				50805B0D1BF2A068005F7A99 /* SPPopUpButtonCell.m in Sources */,
 				173C839111AAD32A00B8B084 /* SPDotExporterDelegate.m in Sources */,
 				173C839211AAD32A00B8B084 /* SPHTMLExporterDelegate.m in Sources */,
 				173C839311AAD32A00B8B084 /* SPPDFExporterDelegate.m in Sources */,