5225ae5b121afcf5c1da8285cc8521b008380eb6
Max <post@wickenrode.com>
2015-11-28 21:18:50 +0100

    Lorem ipsum

--- Source/SPSQLExportHandler.h
+++ Source/SPSQLExportHandler.h
@@ -38,39 +38,3 @@
 @interface SPSQLExportHandler : SPBaseExportHandler <SPExportHandlerInstance>
 
 @end
-
-
-/**
- Don't mind me, I'm just here so git stops thinking a remove+add was a rename
- because of the big similarity in the license header.
- 
- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- Nullam metus est, auctor in dapibus vel, elementum non lacus.
- Curabitur ut lorem non diam vehicula vehicula at ut sem.
- Proin congue vehicula dui non egestas. Mauris non lobortis eros, sit amet malesuada nibh.
- Cras purus nibh, sagittis sed pharetra a, tristique imperdiet ante.
- Phasellus sed rhoncus lectus, ac porttitor est. Aliquam accumsan laoreet nunc,
- quis eleifend nisi mollis ut. In hac habitasse platea dictumst.
- 
- Nunc maximus molestie lorem, ac egestas turpis.
- Duis condimentum interdum justo quis porttitor.
- Suspendisse non dolor in augue pharetra tempus. Nam maximus non ligula ut porttitor.
- Sed ut neque massa. Aenean interdum, nunc fringilla consectetur ultricies,
- lacus magna consectetur enim, eget pretium nulla massa in felis.
- Sed ac cursus leo, ac egestas tellus.
- 
- Aliquam at placerat ante.
- Curabitur sit amet nibh tincidunt, fringilla est eget, scelerisque ante.
- Maecenas a euismod nunc. Morbi porta hendrerit rhoncus. Fusce malesuada nibh lorem,
- sed interdum purus tristique vel. Vestibulum finibus consequat turpis dictum suscipit.
- Sed quis quam pellentesque ipsum dapibus feugiat eu ut nisl. Maecenas vel lorem eu felis
- elementum vulputate eu in leo. Etiam in dui quam. Aenean vitae porttitor massa,
- eget tincidunt magna. Donec finibus facilisis magna,
- id pharetra arcu tempor a.
- Duis cursus magna ut enim posuere rutrum. Nulla scelerisque, est a hendrerit
- consectetur, nisl massa eleifend orci, tincidunt tincidunt nisl enim vitae diam.
- Sed commodo, tortor nec rutrum congue, enim est consectetur ante,
- a bibendum tellus elit id ex. Nulla facilisi.
- Nam nec lacinia nisi, vitae consequat tortor.
- 
- */
