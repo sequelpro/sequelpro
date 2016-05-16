sequelpro/sequelpro

commit b7875e8fdbdc5980c12a847997cc9631979dd4d3

fix warnings

Source/SPDatabaseStructure.m
@@ -152,8 +152,7 @@
 	if([[NSThread currentThread] isCancelled]) goto cleanup_thread_and_pool;
 
 	// This thread is now first on the stack, and about to process the structure.
-#warning Should not set delegate as the notification source object
-	[[NSNotificationCenter defaultCenter] postNotificationName:@"SPDBStructureIsUpdating" object:delegate];
+	[[NSNotificationCenter defaultCenter] postNotificationName:@"SPDBStructureIsUpdating" object:self];
 
 	NSString *connectionID = ([delegate respondsToSelector:@selector(connectionID)])? [NSString stringWithString:[delegate connectionID]] : @"_";
 
@@ -390,8 +389,7 @@ update_globals_and_cleanup:
 
 	if(structureWasUpdated) {
 		// Notify that the structure querying has been performed
-#warning Should not set delegate as the notification source object
-		[[NSNotificationCenter defaultCenter] postNotificationName:@"SPDBStructureWasUpdated" object:delegate];
+		[[NSNotificationCenter defaultCenter] postNotificationName:@"SPDBStructureWasUpdated" object:self];
 	}
 
 cleanup_thread_and_pool:

Source/SPBundleEditorController.m
@@ -701,7 +701,7 @@
 	if ([commandsOutlineView numberOfSelectedRows] != 1) return;
 
 	[[NSWorkspace sharedWorkspace] selectFile:[NSString stringWithFormat:@"%@/%@.%@/%@", 
-		bundlePath, [[self _currentSelectedObject] objectForKey:kBundleNameKey], SPUserBundleFileExtension, SPBundleFileName] inFileViewerRootedAtPath:nil];
+		bundlePath, [[self _currentSelectedObject] objectForKey:kBundleNameKey], SPUserBundleFileExtension, SPBundleFileName] inFileViewerRootedAtPath:@""];
 }
 
 /**

Source/SPConnectionController.m
@@ -2049,6 +2049,28 @@ static NSComparisonResult _compareFavoritesUsingKey(id favorite1, id favorite2, 
 	[super dealloc];
 }
 
+#ifndef SP_CODA
+
+/**
+ * Called by the favorites exporter when the export completes.
+ */
+- (void)favoritesExportCompletedWithError:(NSError *)error
+{
+	if (error) {
+		NSAlert *alert = [NSAlert alertWithMessageText:NSLocalizedString(@"Favorites export error", @"favorites export error message")
+																		 defaultButton:NSLocalizedString(@"OK", @"OK")
+																	 alternateButton:nil
+																			 otherButton:nil
+												 informativeTextWithFormat:NSLocalizedString(@"The following error occurred during the export process:\n\n%@", @"favorites export error informative message"), [error localizedDescription]];
+		
+		[alert beginSheetModalForWindow:[dbDocument parentWindow]
+											modalDelegate:self
+										 didEndSelector:NULL
+												contextInfo:NULL];			
+	}
+}
+#endif
+
 @end
 
 #pragma mark -

Source/SPConnectionControllerDelegate.m
@@ -680,25 +680,6 @@ static NSString *SPQuickConnectImageWhite = @"quick-connect-icon-white.pdf";
 #ifndef SP_CODA
 
 /**
- * Called by the favorites exporter when the export completes.
- */
-- (void)favoritesExportCompletedWithError:(NSError *)error
-{
-	if (error) {
-		NSAlert *alert = [NSAlert alertWithMessageText:NSLocalizedString(@"Favorites export error", @"favorites export error message")
-										 defaultButton:NSLocalizedString(@"OK", @"OK")
-									   alternateButton:nil 
-										   otherButton:nil 
-							 informativeTextWithFormat:NSLocalizedString(@"The following error occurred during the export process:\n\n%@", @"favorites export error informative message"), [error localizedDescription]];
-	
-		[alert beginSheetModalForWindow:[dbDocument parentWindow] 
-						  modalDelegate:self
-						 didEndSelector:NULL
-							contextInfo:NULL];			
-	}
-}
-
-/**
  * Called by the favorites importer when the imported data is available.
  */
 - (void)favoritesImportData:(NSArray *)data

commit 943614e09a2cb7f40efe4bc647b80e25d1cb7b54

Fix an issue with autocompletion not suggesting column names (#2507)
Issue introduced in b7875e8fdbdc5980c12a847997cc9631979dd4d3

Source/SPDatabaseStructure.h
@@ -54,6 +54,7 @@
 
 // Information
 - (SPMySQLConnection *)connection;
+- (SPDatabaseDocument *)delegate;
 
 // Structure retrieval from the server
 - (void)queryDbStructureInBackgroundWithUserInfo:(NSDictionary *)userInfo;

Source/SPDatabaseStructure.m
@@ -127,6 +127,11 @@
 	return [c autorelease];
 }
 
+- (SPDatabaseDocument *)delegate
+{
+	return delegate;
+}
+
 #pragma mark -
 #pragma mark Structure retrieval from the server
 

Source/SPNavigatorController.h
@@ -28,7 +28,7 @@
 //
 //  More info at <https://github.com/sequelpro/sequelpro>
 
-@class SPNavigatorOutlineView, SPSplitView;
+@class SPNavigatorOutlineView, SPSplitView, SPDatabaseDocument;
 
 @interface SPNavigatorController : NSWindowController 
 {
@@ -79,7 +79,7 @@
 - (void)reloadAfterFiltering;
 - (IBAction)syncButtonAction:(id)sender;
 
-- (void)updateEntriesForConnection:(id)object;
+- (void)updateEntriesForConnection:(SPDatabaseDocument *)doc;
 - (NSString*)tableInfoLabelForIndex:(NSInteger)index ofType:(NSInteger)type;
 
 - (void)updateNavigator:(NSNotification *)aNotification;

Source/SPNavigatorController.m
@@ -168,8 +168,7 @@ static NSComparisonResult compareStrings(NSString *s1, NSString *s2, void* conte
 	functionIcon = [[NSImage imageNamed:@"func-small"] retain];
 	fieldIcon = [[NSImage imageNamed:@"field-small-square"] retain];
 
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateNavigator:)
-												 name:@"SPDBStructureWasUpdated" object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateNavigator:) name:@"SPDBStructureWasUpdated" object:nil];
 
 }
 
@@ -408,16 +407,12 @@ static NSComparisonResult compareStrings(NSString *s1, NSString *s2, void* conte
 
 - (void)updateNavigator:(NSNotification *)aNotification
 {
+	SPDatabaseDocument *object = [(SPDatabaseStructure *)[aNotification object] delegate];
 
-	id object = [aNotification object];
-
-	if([object isKindOfClass:[SPDatabaseDocument class]])
-		[self performSelectorOnMainThread:@selector(updateEntriesForConnection:) withObject:object waitUntilDone:NO];
-	else
-		[self performSelectorOnMainThread:@selector(updateEntriesForConnection:) withObject:nil waitUntilDone:NO];
+	[self performSelectorOnMainThread:@selector(updateEntriesForConnection:) withObject:object waitUntilDone:NO];
 }
 
-- (void)updateEntriesForConnection:(id)doc
+- (void)updateEntriesForConnection:(SPDatabaseDocument *)doc
 {
 
 	if(ignoreUpdate) {
@@ -431,8 +426,7 @@ static NSComparisonResult compareStrings(NSString *s1, NSString *s2, void* conte
 		[cachedSortedKeys removeAllObjects];
 	}
 
-
-	if (doc && [doc isKindOfClass:[SPDatabaseDocument class]]) {
+	if (doc) {
 
 		SPMySQLConnection *theConnection = [doc getConnection];
 		if(!theConnection || ![theConnection isConnected]) return;
@@ -488,7 +482,7 @@ static NSComparisonResult compareStrings(NSString *s1, NSString *s2, void* conte
 	if(isFiltered && [[self window] isVisible])
 		[self filterTree:self];
 
-	[[NSNotificationCenter defaultCenter] postNotificationName:@"SPNavigatorStructureWasUpdated" object:doc];
+	[[NSNotificationCenter defaultCenter] postNotificationName:@"SPNavigatorStructureWasUpdated" object:self];
 
 }
 