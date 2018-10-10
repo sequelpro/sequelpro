sequelpro/sequelpro

commit c37bea80c8871ed8092da9152b89d2dc3607c6fd

#3213: Fix exception by ensuring SPBeginAlertSheet is always called on the main thread.

Source/SPAlertSheets.m
@@ -207,30 +207,34 @@ void SPBeginAlertSheet(
 		void *contextInfo,
 	NSString *msg) 
 {
-	NSButton *aButton;
+	dispatch_sync(dispatch_get_main_queue(), ^{
+		// Set up an NSAlert with the supplied details
+		NSAlert *alert = [[[NSAlert alloc] init] autorelease];
 
-	// Set up an NSAlert with the supplied details
-	NSAlert *alert = [[[NSAlert alloc] init] autorelease];
-	[alert setMessageText:title];
-	aButton = [alert addButtonWithTitle:defaultButton];
-	[aButton setTag:NSAlertDefaultReturn];
+		[alert setMessageText:title];
 
-	// Add 'alternate' and 'other' buttons as appropriate
-	if (alternateButton) {
-		aButton = [alert addButtonWithTitle:alternateButton];
-		[aButton setTag:NSAlertAlternateReturn];
-	}
-	if (otherButton) {
-		aButton = [alert addButtonWithTitle:otherButton];
-		[aButton setTag:NSAlertOtherReturn];
-	}
+		NSButton *aButton = [alert addButtonWithTitle:defaultButton];
 
-	// Set the informative message if supplied
-	if (msg) [alert setInformativeText:msg];
+		[aButton setTag:NSAlertDefaultReturn];
 
-	// Run the alert on the main thread
-	[[alert onMainThread] beginSheetModalForWindow:docWindow modalDelegate:modalDelegate didEndSelector:didEndSelector contextInfo:contextInfo];
+		// Add 'alternate' and 'other' buttons as appropriate
+		if (alternateButton) {
+			aButton = [alert addButtonWithTitle:alternateButton];
+			[aButton setTag:NSAlertAlternateReturn];
+		}
 
-	// Ensure the alerting window is frontmost
-	[[docWindow onMainThread] makeKeyWindow];
+		if (otherButton) {
+			aButton = [alert addButtonWithTitle:otherButton];
+			[aButton setTag:NSAlertOtherReturn];
+		}
+
+		// Set the informative message if supplied
+		if (msg) [alert setInformativeText:msg];
+
+		// Run the alert on the main thread
+		[alert beginSheetModalForWindow:docWindow modalDelegate:modalDelegate didEndSelector:didEndSelector contextInfo:contextInfo];
+
+		// Ensure the alerting window is frontmost
+		[docWindow makeKeyWindow];
+	});
 }

commit 102f6efbb20e86173e85974beadf2dc84dc7d694

#3213: Fix crash caused by c37bea8.

Source/SPAlertSheets.m
@@ -30,6 +30,7 @@
 
 #import "SPAlertSheets.h"
 #import "SPMainThreadTrampoline.h"
+#import "SPFunctions.h"
 
 @implementation SPAlertSheets
 
@@ -207,7 +208,7 @@ void SPBeginAlertSheet(
 		void *contextInfo,
 	NSString *msg) 
 {
-	dispatch_sync(dispatch_get_main_queue(), ^{
+	void (^alertBlock)(void) = ^{
 		// Set up an NSAlert with the supplied details
 		NSAlert *alert = [[[NSAlert alloc] init] autorelease];
 
@@ -236,5 +237,12 @@ void SPBeginAlertSheet(
 
 		// Ensure the alerting window is frontmost
 		[docWindow makeKeyWindow];
-	});
+	};
+
+	if (dispatch_get_current_queue() == dispatch_get_main_queue()) {
+		alertBlock();
+	}
+	else {
+		dispatch_sync(dispatch_get_main_queue(), alertBlock);
+	}
 }