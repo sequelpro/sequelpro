cd82d43bd9f66f2d67cbd45c86650fbb63eaa59c
Noah <noah@hack.se>
2019-07-01 15:58:10 +0200

    SPMySQLFramework: upgrade libmysqlclient.a for TLSv1.1 support
    MariaDB 10.4.6 (GA as of June, 2019) replaces the yaSSL embedded TLS library
    with wolfSSL and drops support for TLS 1.0 in its default configuration
    (`tls_version=TLSv1.1,TLSv1.2,TLSv1.3`).
    
    Before MySQL 5.7, the embedded yaSSL library used by libmysqlclient.a only
    supported TLS 1.0.
    
    This changeset upgrades the MySQL client library to 5.7 to allow SequelPro
    to connect to TLS protected MariaDB 10.4 servers.
    
    - upgrade libmysqlclient.a and includes from 5.5.56 to 5.7.26
    - drop custom mysql patches (all types are present in MySQL 5.6 and 5.7)
    - add `-lc++` to SPMySQLFramework linker flags
    - add `-b <boost source path>` option to mysqlclient build script
    
    Fixes #3492.

--- Frameworks/SPMySQLFramework/MySQL Client Libraries/include/mysql_time.h
+++ Frameworks/SPMySQLFramework/MySQL Client Libraries/include/mysql_time.h
@@ -1,5 +1,4 @@
-/* Copyright (c) 2004, 2006 MySQL AB
-   Use is subject to license terms
+/* Copyright (c) 2004, 2011, Oracle and/or its affiliates. All rights reserved.
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -12,7 +11,7 @@
 
  You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA */
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA */
 
 #ifndef _mysql_time_h_
 #define _mysql_time_h_
@@ -48,7 +47,7 @@ enum enum_mysql_timestamp_type
 typedef struct st_mysql_time
 {
   unsigned int  year, month, day, hour, minute, second;
-  unsigned long second_part;
+  unsigned long second_part;  /**< microseconds */
   my_bool       neg;
   enum enum_mysql_timestamp_type time_type;
 } MYSQL_TIME;
